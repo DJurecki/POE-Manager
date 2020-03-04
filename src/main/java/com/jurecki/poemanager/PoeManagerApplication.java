865fc66e59d28a4794adaebc2c083aeb968e648c
DJurecki <dejwux1@gmail.com>
2020-03-04 17:23:29 +0100

    Added project structure.

---
+++ src/main/java/com/jurecki/poemanager/PoeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jurecki.poemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoeManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jurecki/poemanager/PoeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jurecki.poemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
